5086add0a36d4bdfd287b5d9402d4b787df6e38f
SaurabhGuchait <saurabhguchait@gmail.com>
2024-12-03 07:18:25 +0000

    Maths

---
+++ 01-basics/06_maths.js
@@ -0,0 +1,28 @@
+const number = 100;
+const balance = new Number(200.00);
+// console.log(balance);//[Number: 200]
+// console.log(balance.toString());//200
+// console.log(typeof balance);//object
+// console.log(typeof balance.toString());//string
+// console.log(balance.toFixed(2));
+
+// const hundereds = 100000000000;
+// console.log(hundereds.toLocaleString("en-IN"));
+
+/******Maths*******/
+
+// console.log(Math.abs(-4));// Absolute value - Negative to Positive
+// console.log(Math.round(4.789));//5
+// console.log(Math.sqrt(49));//7
+
+console.log(Math.random());//Values between 0 and 1
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10;
+const max = 20;
+Math.floor(Math.random() * (max - min + 1) + min)
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
+
+
+
