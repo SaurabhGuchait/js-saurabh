500453be425078df9fb1c73e0fbf2476700ce820
SaurabhGuchait <saurabhguchait@gmail.com>
2024-12-03 06:44:01 +0000

    Strings

---
+++ 01-basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Saurabh"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");// Saurabh50 Value - Outdated
+
+//console.log(`My name is ${name} and my repo count is ${repoCount}`);// String Interpolation
+
+// const newName = new String("Saurabh");
+// console.log(newName);// [String: 'Saurabh']
+// console.log(newName[1]);//a
+// console.log(newName.__proto__);//{}
+// console.log(newName.length);//7
+// console.log(newName.toUpperCase());//SAURABH
+// console.log(newName.substring(0,4));//Saur - goes from 0 to 3 and not till 4
+// console.log(newName.slice(0,4));//Saur - we can use negative values
+
+// const newName = "    Saurabh    "
+// console.log(newName);
+// console.log(newName.trim());// Saurabh - removes all white spaces and new line
+
+// const url = "https://google.com/saurabh%20guchait";
+// console.log(url.replace("%20","-"));//replaces %20 with -
+// console.log(url.includes("saurabh"));//true
+
+// const newName = "Guchait-is-good";
+// console.log(newName.split("-"));// gives us with an array ['Guchait', 'is', 'good']
+
+
+
+
+
+
+
+
+
