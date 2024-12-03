5fddf5b72035520e673cfaac3fef9e77d2029956
SaurabhGuchait <saurabhguchait@gmail.com>
2024-12-03 04:03:41 +0000

    Conversion and Operation

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -3,24 +3,60 @@
 //let score = undefined;
 //let score = true;
 let score = "true";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);// NaN - Not a Number
-console.log(valueInNumber);// 0 - In case of null
-console.log(valueInNumber);// NaN - In case of undefined
-console.log(valueInNumber);// 1 - In case of boolean
-console.log(valueInNumber);// NaN - In case of string
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// NaN - Not a Number
+// console.log(valueInNumber);// 0 - In case of null
+// console.log(valueInNumber);// NaN - In case of undefined
+// console.log(valueInNumber);// 1 - In case of boolean
+// console.log(valueInNumber);// NaN - In case of string
 
 //let isLoggedIn = 1;
 //let isLoggedIn = "";
 let isLoggedIn = "Saurabh"
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);// true - for 1
-console.log(booleanIsLoggedIn);// false - ""
-console.log(booleanIsLoggedIn);// true - "Saurabh"
+// console.log(booleanIsLoggedIn);// true - for 1
+// console.log(booleanIsLoggedIn);// false - ""
+// console.log(booleanIsLoggedIn);// true - "Saurabh"
+
+/*********Operations*********/
+let value = 13;
+let negValue = -value;
+//console.log(negValue);// -13
+
+//console.table([2+2, 2-2, 2*2, 2**3, 2/2, 2%3]); //2**3 - power of
+ let str1 = "Saurabh";
+ let str2 = " Guchait"
+ let str3 = str1+str2
+ //console.log(str3);// Saurabh Guchait
+
+//  console.log("1" + 2);// 12
+//  console.log(1 + "2");// 12
+//  console.log("1" + 2 + 2);// 122 - String comes first then entire things is considered as String
+//  console.log(1 + 2 + "2");// 32 - If string comes at last then entire operation happens first and then string get attached to it.
+
+//console.log(+true);// 1
+
+let counter = 100;
+// console.log(counter++);//100
+// console.log(counter);//101
+
+let counter1 = 100;
+console.log(++counter1);//101
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+
+
 
 
 
