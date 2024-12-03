d3859aa57819038d026330fc80e010c1e820e62c
SaurabhGuchait <saurabhguchait@gmail.com>
2024-12-03 03:39:34 +0000

    Conversions

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = "33abc";
+//let score = null;
+//let score = undefined;
+//let score = true;
+let score = "true";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// NaN - Not a Number
+console.log(valueInNumber);// 0 - In case of null
+console.log(valueInNumber);// NaN - In case of undefined
+console.log(valueInNumber);// 1 - In case of boolean
+console.log(valueInNumber);// NaN - In case of string
+
+//let isLoggedIn = 1;
+//let isLoggedIn = "";
+let isLoggedIn = "Saurabh"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);// true - for 1
+console.log(booleanIsLoggedIn);// false - ""
+console.log(booleanIsLoggedIn);// true - "Saurabh"
+
+
+
+
+
+
+
