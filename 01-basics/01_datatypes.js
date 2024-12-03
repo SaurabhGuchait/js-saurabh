e91ea547ad33192c7dc2c0d9fabf750b9e848961
SaurabhGuchait <saurabhguchait@gmail.com>
2024-12-03 03:02:50 +0000

    Datatypes

--- 01-basics/01_datatypes.js
+++
@@ -1,8 +0,0 @@
-"use strict"//treat all JS code as newer versions
-//alert(3+3)//we are uisng nodejd and not browser
-
-let name = "Saurabh"; //String
-let age = 18; //Number
-let isTrue = false //Boolean
-
-//number = 2 to the power 53 
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all JS code as newer versions
+//alert(3+3)//we are using nodejs and not browser
+
+let name = "Saurabh"; //String
+let age = 18; //Number
+let isTrue = false //Boolean
+
+//number = 2 to the power 53 
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => no value assigned
+//symbol => To identify unique component
+
+//object
+
+console.log(typeof age); //number
+console.log(typeof "Saurabh"); //string
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
